68a7f206f67753846c2b3087484932f832071709
Clément Sapt <csapt2001@gmail.com>
2022-02-02 15:20:56 +0100

    install sidekiq :construction:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
+  # skip_before_action :verify_authenticity_token
   def create
     @user = User.new(user_params)
     if @user.save
       UserMailer.with(user: @user).welcome_email.deliver_later
+      redirect_to root_path
     end
   end
 
