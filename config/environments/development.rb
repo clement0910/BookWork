d6a9052695d687d93f0dcbdf8d8523b552a45b71
Clément Sapt <csapt2001@gmail.com>
2022-02-02 14:34:03 +0100

    add active mailer configuration and send first email :card_file_box:

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: ENV['ADDRESS'],
+    port: ENV['PORT'],
+    domain: ENV['DOMAIN'],
+    user_name: ENV['USERNAME'],
+    password: ENV['PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    open_timeout: 5,
+    read_timeout: 5
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
